8fd69b4d2187ac27e6d729fdb489dbb7113cb455
Vahid Ghanbarpour <vahidgr.vahid@gmail.com>
2019-10-21 15:52:08 +0330

    Initial Commit

---
+++ BreakingNews/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BreakingNews
+//
+//  Created by Vahid Ghanbarpour on 10/21/19.
+//  Copyright © 2019 Vahid Ghanbarpour. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
